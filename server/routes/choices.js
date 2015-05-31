41c7dbcae000edf8ca0128621c4d3372875dbd2e
Donghyun Go <godong9@gmail.com>
2015-05-31 11:27:10 +0900

    Update choices routes

--- server/routes/choices.js
+++ server/routes/choices.js
@@ -2,6 +2,23 @@ var express = require('express');
 var router = express.Router();
 var choiceCtrl = require('../controllers/Choice');
 
+
+/**
+ * ---------
+ * ## **GET choice**
+ *  - 선택 항목 자세한 정보 가져오는 API
+ *
+ * ### URL: /ajax/choice
+ * ### TYPE: GET
+ *
+ * @example
+ *  REQUEST
+ *   - GET
+ *  RESPONSE
+ *   -
+ */
+router.get('/id/:id', choiceCtrl.getChoice);
+
 /**
  * ---------
  * ## **GET choices**
@@ -24,8 +41,34 @@ var choiceCtrl = require('../controllers/Choice');
  */
 router.get('/list', choiceCtrl.getChoices);
 
-router.get('/id/:id', choiceCtrl.getChoice);
-
+/**
+ * ---------
+ * ## **POST choice**
+ *  - 선택 항목 저장하는 API
+ *
+ * ### URL: /ajax/choice
+ * ### TYPE: POST
+ *
+ * @param {String} req.body.title - 제목
+ * @param {String} [req.body.description] - 자세한 내용
+ * @param {Array} [req.body.tags] - 태그 목록
+ * @param {JSONObject} req.body.item1 - 아이템1에 해당하는 정보
+ * @param {String} req.body.item1.name - 아이템1 이름
+ * @param {String} [req.body.item1.image] - 아이템1 이미지 URL
+ * @param {JSONObject} req.body.item2 - 아이템2에 해당하는 정보
+ * @param {String} req.body.item2.name - 아이템2 이름
+ * @param {String} [req.body.item2.image] - 아이템2 이미지 URL
+ * @param {JSONObject} [req.body.item3] - 아이템3에 해당하는 정보
+ * @param {String} [req.body.item3.name] - 아이템3 이름
+ * @param {String} [req.body.item3.image] - 아이템3 이미지 URL
+ *
+ * @example
+ *  REQUEST
+ *   - POST /ajax/choice
+ *  RESPONSE
+ *   - 
+ *
+ */
 router.post('/', choiceCtrl.saveChoice);
 
 
