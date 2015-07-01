0d4c9b50c972d8ad6e36c5f71cf0eb69bbfd28bb
Donghyun Go <godong9@gmail.com>
2015-07-01 22:09:48 +0900

    Update game

--- server/public/javascripts/gamePage.js
+++ server/public/javascripts/gamePage.js
@@ -1,9 +1,19 @@
 (function main() {
     'use strict';
 
-
     function init() {
-
+        setTimeout(function(){
+            console.log("index:1");
+            $('#game_img').attr('src', '/images/eyes-02.png');
+            setTimeout(function(){
+                console.log("index:2");
+                $('#game_img').attr('src', '/images/eyes-03.png');
+                setTimeout(function(){
+                    console.log("index:3");
+                    $('#game_img').attr('src', '/images/eyes-04.png');
+                }, 1000);
+            }, 1000);
+        }, 1000);
     }
 
     init();
